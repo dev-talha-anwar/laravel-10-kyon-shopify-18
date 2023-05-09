2a6a43216ac26572423873bf3b4085a415ea9e65
dev-talha-anwar <56028813+dev-talha-anwar@users.noreply.github.com>
2023-05-09 12:47:22 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Osiset\BasicShopifyAPI\Options;
+use Osiset\BasicShopifyAPI\BasicShopifyAPI;
+use function Osiset\ShopifyApp\getShopifyConfig;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +23,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    public static function shopifyApiClosure (Options $opts) {
+        $ts = config('shopify-app.api_time_store');
+        $ls = config('shopify-app.api_limit_store');
+        $sd = config('shopify-app.api_deferrer');
+
+        // Custom Guzzle options
+        $opts->setGuzzleOptions(['timeout' => 90.0]);
+       
+        return new BasicShopifyAPI(
+            $opts,
+            new $ts(),
+            new $ls(),
+            new $sd()
+        );
     }
 }
